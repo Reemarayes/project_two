464e8d28233a430dceb7ee08b5acc8fa452fc249
REEMA <reema.rayes@gmail.com>
2019-04-03 17:01:15 +0300

    Updates

---
+++ precache-manifest.ea68ebc642cab06eb04834219aad664d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "949f2bd6a51560bc6246",
+    "url": "/project_two/static/js/runtime~main.94a91d53.js"
+  },
+  {
+    "revision": "333ad67cca08ece5f5f0",
+    "url": "/project_two/static/js/main.444c2f49.chunk.js"
+  },
+  {
+    "revision": "b8de1f0fe4631c47d990",
+    "url": "/project_two/static/js/2.7102c60e.chunk.js"
+  },
+  {
+    "revision": "333ad67cca08ece5f5f0",
+    "url": "/project_two/static/css/main.1680f340.chunk.css"
+  },
+  {
+    "revision": "cceff367540309f52c7dd75e928f8ecd",
+    "url": "/project_two/index.html"
+  }
+];
